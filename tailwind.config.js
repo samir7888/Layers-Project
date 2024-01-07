cb967352fbe39e5ee2bc08b5e794b4f58a8e16c8
samir7888 <basnetsameer78@gmail.com>
2024-01-07 13:33:51 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens :{
+        "small": {'max' : "950px"},
+        "vsmall": {'max' : "768px"},
+        "extrasmall": {'max' : "430px"},
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
